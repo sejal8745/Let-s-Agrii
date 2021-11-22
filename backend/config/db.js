a7db38338244a46d12bf6b3ec3580bbfed2e7610
Parul <parulsurana28@gmail.com>
2021-11-22 21:19:42 +0530

    Done Mongoose Connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect('mongodb+srv://seProject:letsagrii@cluster0.ysgwm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
                 useNewUrlParser: true,
                 useCreateIndex: true,
                 useFindAndModify: false,
--- frontend/src/screens/Home/HomeScreen.jsx
+++ frontend/src/screens/Home/HomeScreen.jsx
@@ -14,7 +14,7 @@ const HomeScreen = () => {
             <SliderComponent />
             <CardMenu />
             <OurServices />
-            <GenralMap />
+            {/* <GenralMap /> */}
         </>
     )
 }
