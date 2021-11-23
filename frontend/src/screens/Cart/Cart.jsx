823752bae008eb90d8a434845e1338d93a564477
Harshita verma <56623934+Harshitavkb26@users.noreply.github.com>
2021-11-23 22:28:33 +0530

    Added Cart screen

--- frontend/src/screens/Cart/Cart.jsx
+++ frontend/src/screens/Cart/Cart.jsx
@@ -33,7 +33,7 @@ const Cart = ({ match, location, history }) => {
     return (
         <Container style={{ marginTop: '100px', marginBottom: '50px' }}>
             <Meta
-                title="Agroic | Cart"
+                title="Let's Agrii | Cart"
             />
             <Row>
                 <Col md={8}>
