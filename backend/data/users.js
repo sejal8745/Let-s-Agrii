753fc4bdff95f99993f958386b2067da26059d5c
sejalgoyal <sejalgoyal16@gmail.com>
2021-11-24 17:06:14 +0530

    Created dummy data

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,25 +1,31 @@
-import bcrypt from 'bcryptjs'
+import bcrypt from "bcryptjs";
 
 const users = [
-    {
-        name: 'Admin User',
-        email: 'admin@example.com',
-        password: bcrypt.hashSync('123456', 10),
-        cropSelection: '',
-        isAdmin: true
-    },
-    {
-        name: 'Sanjula User',
-        email: 'Sanjula@example.com',
-        cropSelection: 'paddy',
-        password: bcrypt.hashSync('123456', 10),
-    },
-    {
-        name: 'Test User',
-        email: 'Test@example.com',
-        cropSelection: 'fruits',
-        password: bcrypt.hashSync('123456', 10),
-    },
-]
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: bcrypt.hashSync("123456", 10),
+    cropSelection: "",
+    isAdmin: true,
+  },
+  {
+    name: "Sanjula User",
+    email: "Sanjula@example.com",
+    cropSelection: "paddy",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Test User",
+    email: "Test@example.com",
+    cropSelection: "fruits",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Smarty",
+    email: "smarty2310@example.com",
+    cropSelection: "wheat",
+    password: bcrypt.hashSync("123456", 10),
+  },
+];
 
-export default users
+export default users;
